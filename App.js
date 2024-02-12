379ff82eba9ddfbea3f57571acb8d04047cffc4d
SuviAnnina <suvi.varjoranta@gmail.com>
2024-02-12 20:30:09 +0200

    first add

--- App.js
+++ App.js
@@ -1,10 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [search, setSearch] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchRecipes = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`)
+      .then(response => {
+        if (!response)
+          throw new Error('Error in fetch: ' + response.statusText);
+        return response.json();
+      })
+      .then(data => {
+        setRecipes(data.meals);
+        console.log("joujee fetchi jepa")
+        console.log(search)
+
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flex: 1, marginTop: 100 }}>
+        <TextInput
+          placeholder='Type an ingredient'
+          value={search}
+          onChangeText={text => setSearch(text)}
+        />
+        <Button
+          title="Find"
+          onPress={fetchRecipes}
+        />
+      </View>
+
+      <View style={{ flex: 6 }}>
+        <FlatList
+          data={recipes}
+          renderItem={({ item }) =>
+            <View style={{ margin: 10 }}>
+              <Text>{item.strMeal}</Text>
+
+            </View>
+          }
+        />
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
